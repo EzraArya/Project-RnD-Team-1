0847cb20e65c4caa2de9dad00c8c2d0f29036b1d
Reynaldy Marchell <reynalldymarchell03@gmail.com>
2022-12-14 02:54:36 +0700

    Create Duplicate Button JS and style css
    menambahkan duplicate button pada javascript dan melakukan sedikit style css.

--- main.js
+++ main.js
@@ -35,11 +35,15 @@ window.addEventListener('load', () => {
 		task_delete_el.classList.add('delete');
 		task_delete_el.innerText = 'Delete';
 
+		const task_duplicate_el = document.createElement('button');
+		task_duplicate_el.classList.add('duplicate');
+		task_duplicate_el.innerText = 'Duplicate';
+
 		task_actions_el.appendChild(task_edit_el);
 		task_actions_el.appendChild(task_delete_el);
+		task_actions_el.appendChild(task_duplicate_el);
 
 		task_el.appendChild(task_actions_el);
-
 		list_el.appendChild(task_el);
 
 		input.value = '';
@@ -55,6 +59,67 @@ window.addEventListener('load', () => {
 			}
 		});
 
-            input.value = ''
-        })
-    })
+		task_delete_el.addEventListener('click', (e) => {
+			list_el.removeChild(task_el);
+		});
+
+		task_duplicate_el.addEventListener('click', (e) => {
+			const task = input.value;
+
+		const task_el = document.createElement('div');
+		task_el.classList.add('task');
+
+		const task_content_el = document.createElement('div');
+		task_content_el.classList.add('content');
+
+		task_el.appendChild(task_content_el);
+
+		const task_input_el = document.createElement('input');
+		task_input_el.classList.add('text');
+		task_input_el.type = 'text';
+		task_input_el.value = task;
+		task_input_el.setAttribute('readonly', 'readonly');
+
+		task_content_el.appendChild(task_input_el);
+
+		const task_actions_el = document.createElement('div');
+		task_actions_el.classList.add('actions');
+		
+		const task_edit_el = document.createElement('button');
+		task_edit_el.classList.add('edit');
+		task_edit_el.innerText = 'Edit';
+
+		const task_delete_el = document.createElement('button');
+		task_delete_el.classList.add('delete');
+		task_delete_el.innerText = 'Delete';
+
+		const task_duplicate_el = document.createElement('button');
+		task_duplicate_el.classList.add('duplicate');
+		task_duplicate_el.innerText = 'Duplicate';
+
+		task_actions_el.appendChild(task_edit_el);
+		task_actions_el.appendChild(task_delete_el);
+		task_actions_el.appendChild(task_duplicate_el);
+
+		task_el.appendChild(task_actions_el);
+		list_el.appendChild(task_el);
+
+		input.value = '';
+
+		task_edit_el.addEventListener('click', (e) => {
+			if (task_edit_el.innerText.toLowerCase() == "edit") {
+				task_edit_el.innerText = "Save";
+				task_input_el.removeAttribute("readonly");
+				task_input_el.focus();
+			} else {
+				task_edit_el.innerText = "Edit";
+				task_input_el.setAttribute("readonly", "readonly");
+			}
+		});
+
+		task_delete_el.addEventListener('click', (e) => {
+			list_el.removeChild(task_el);
+		});
+		});
+	});
+});
